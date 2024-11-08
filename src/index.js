0e239ff937d1c2dbb80bdda68c9835fd636e82c8
maxheerdegen <96018857+maxheerdegen@users.noreply.github.com>
2024-11-08 09:14:13 +0100

    add object for ToDos

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import "./styles.css";
+import { createToDo, addToDo, projects, projectNames } from "./logic.js";
 
+
+addToDo("study", "programming", "tomorrow", "high");
+addToDo("eat", "cook", "today", "high", "diet");
+
+
+console.log(projects);
+console.log(projectNames);
--- src/logic.js
+++ src/logic.js
@@ -1,15 +1,23 @@
-function createToDo(title, description, dueDate, priority) {
+function createToDo(title, description, dueDate, priority, project) {
     const toDoTitle = title;
     const toDoDescription = description;
     const toDoDueDate = dueDate;
-    const toDOPriority = priority;
+    const toDoPriority = priority;
+    const toDoProject = project;
 
-    return {toDoTitle, toDoDescription, toDoDueDate, toDOPriority};
+    return {toDoTitle, toDoDescription, toDoDueDate, toDoPriority, toDoProject};
 }
 
-function addToDo () {
-    const toDo = createToDo(title, description, dueDate, priority);
-    defaultProjects.push(toDO);  
+function addToDo (title, description, dueDate, priority, project) {
+    const toDo = createToDo(title, description, dueDate, priority, project);
+    projects.push(toDo);
+
+    if (!projectNames.includes(project) && !project=="") {
+        projectNames.push(project);
+    }
 }
 
-defaultProjects = [];
+const projects = [];
+const projectNames = [];
+
+export { createToDo, addToDo, projects, projectNames };
