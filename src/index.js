94a45bd143ece4c11230148f7f4c8099e1efd6f0
maxheerdegen <96018857+maxheerdegen@users.noreply.github.com>
2024-11-08 11:47:01 +0100

    add changeable priority to ToDo object

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import "./styles.css";
 import { createToDo, addToDo, projects, projectNames } from "./logic.js";
 
-
+const firstToDo = createToDo("study", "programming", "tomorrow", "high");
 addToDo("study", "programming", "tomorrow", "high");
-addToDo("eat", "cook", "today", "high", "diet");
+addToDo("eat", "cook", "today", "medium", "diet");
 
 
-console.log(projects);
+console.log(projects[0].getPriority());
+console.log(projects[1].getPriority());
+
+projects[0].changePriority("low");
+console.log(projects[0].getPriority());
+
+console.table(projects);
 console.log(projectNames);
+console.log(firstToDo)
--- src/logic.js
+++ src/logic.js
@@ -1,11 +1,14 @@
 function createToDo(title, description, dueDate, priority, project) {
-    const toDoTitle = title;
-    const toDoDescription = description;
-    const toDoDueDate = dueDate;
-    const toDoPriority = priority;
+    let toDoTitle = title;
+    let toDoDescription = description;
+    let toDoDueDate = dueDate;
+    let toDoPriority = priority;
     const toDoProject = project;
 
-    return {toDoTitle, toDoDescription, toDoDueDate, toDoPriority, toDoProject};
+    const getPriority = () => toDoPriority;
+    const changePriority = (newPriority) => toDoPriority = newPriority;
+
+    return {toDoTitle, toDoDescription, toDoDueDate, toDoProject, getPriority, changePriority};
 }
 
 function addToDo (title, description, dueDate, priority, project) {
