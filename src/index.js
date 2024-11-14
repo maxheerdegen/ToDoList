9a6310d153947c32f4852dee895550b6bf328e50
maxheerdegen <96018857+maxheerdegen@users.noreply.github.com>
2024-11-14 16:55:26 +0100

    add basic interface

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createToDo, addToDo, projects, projectNames } from "./logic.js";
 const firstToDo = createToDo("study", "programming", "tomorrow", "high");
 addToDo("study", "programming", "tomorrow", "high");
 addToDo("eat", "cook", "today", "medium", "diet");
+addToDo("sleep", "get recovery", "now", "high");
 
 
 console.log(projects[0].getPriority());
@@ -15,3 +16,23 @@ console.log(projects[0].getPriority());
 console.table(projects);
 console.log(projectNames);
 console.log(firstToDo)
+
+
+const container = document.querySelector(".container");
+
+window.onload = () => {
+    for (let i = 0; i < projects.length; i++) {
+        const cardContent = document.createElement("div");
+        const title = document.createElement("div");
+        const description = document.createElement("div");
+        const dueDate = document.createElement("div");
+        const priority = document.createElement("div");
+
+        title.textContent = `Title: ${projects[i].toDoTitle}`;
+        description.textContent = `Description: ${projects[i].toDoDescription}`; 
+        dueDate.textContent = `Due Date: ${projects[i].toDoDueDate}`; 
+        priority.textContent = `Priority: ${projects[i].toDoPriority}`; 
+
+        
+    }
+};
