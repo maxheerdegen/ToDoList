34b4bcea6fb1ee7e49d1e2e8708e7d82825fa93f
maxheerdegen <maxheerdegen@max.com>
2024-11-19 11:06:10 +0100

    add functions to modal

--- src/index.js
+++ src/index.js
@@ -1,38 +1,47 @@
 import "./styles.css";
 import { createToDo, addToDo, projects, projectNames } from "./logic.js";
-
-const firstToDo = createToDo("study", "programming", "tomorrow", "high");
-addToDo("study", "programming", "tomorrow", "high");
-addToDo("eat", "cook", "today", "medium", "diet");
-addToDo("sleep", "get recovery", "now", "high");
+import { newToDoForm, newToDoDialog, confirmButton, modalTitle, modalDescription, modalDueDate, modalPriority, modalProjectName } from "./modal.js";
 
 
-const container = document.querySelector(".container");
-
-window.onload = () => {
+function displayToDos() {
+    
+    while (container.lastChild){
+        container.removeChild(container.lastChild);
+    }
+    
     for (let i = 0; i < projects.length; i++) {
         const cardContent = document.createElement("div");
         const title = document.createElement("div");
         const description = document.createElement("div");
         const dueDate = document.createElement("div");
         const priority = document.createElement("div");
-
+        
         title.textContent = `Title: ${projects[i].toDoTitle}`;
         description.textContent = `Description: ${projects[i].toDoDescription}`; 
         dueDate.textContent = `Due Date: ${projects[i].toDoDueDate}`; 
         priority.textContent = `Priority: ${projects[i].getPriority()}`;
-
+        
         cardContent.classList.toggle("card");
-
+        
         cardContent.append(title, description, dueDate, priority);
         container.appendChild(cardContent);
     }
 };
 
-const showDialogButton = document.querySelector(".showDialog");
-const newToDoDialog = document.querySelector(".newToDoDialog");
+const firstToDo = createToDo("study", "programming", "tomorrow", "high");
+addToDo("study", "programming", "tomorrow", "high");
+addToDo("eat", "cook", "today", "medium", "diet");
+addToDo("sleep", "get recovery", "now", "high");
 
-showDialogButton.addEventListener("click", () => {
-    newToDoDialog.showModal();
+const container = document.querySelector(".container");
+
+confirmButton.addEventListener("click", () => {
+    event.preventDefault();
+    addToDo(modalTitle.value, modalDescription.value, modalDueDate.value, modalPriority.value, modalProjectName.value);
+    displayToDos();
+    newToDoForm.reset();
+    newToDoDialog.close();
 })
+
+window.onload = displayToDos();
 
---
+++ src/modal.js
@@ -0,0 +1,33 @@
+const showDialogButton = document.querySelector(".showDialog");
+const newToDoDialog = document.querySelector(".newToDoDialog");
+const cancelButton = document.querySelector(".cancel");
+const newToDoForm = document.querySelector(".newToDoForm");
+const confirmButton = document.querySelector(".confirm");
+const modalTitle = document.querySelector("#title");
+const modalDescription = document.querySelector("#description");
+const modalDueDate = document.querySelector("#dueDate");
+const modalPriority = document.querySelector("#priority");
+const projectCheckbox = document.querySelector("#projectCheckbox");
+const modalProjectName = document.querySelector("#project");
+
+
+showDialogButton.addEventListener("click", () => {
+    newToDoDialog.showModal();
+})
+
+cancelButton.addEventListener("click", () => {
+    modalProjectName.disabled = true;
+    newToDoForm.reset();
+})
+
+projectCheckbox.addEventListener("click", () => {
+    if (projectCheckbox.checked) {
+        modalProjectName.disabled = false;
+    }
+    else {
+        modalProjectName.value = "";
+        modalProjectName.disabled = true;
+    }
+})
+
+export { newToDoForm, newToDoDialog, confirmButton, modalTitle, modalDescription, modalDueDate, modalPriority, modalProjectName };
